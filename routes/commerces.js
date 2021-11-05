a9867d21ee285da7ea229a51a4b68958537fb1b4
berf2608 <frederic.bergeron2@usherbrooke.ca>
2021-11-04 23:06:05 -0400

    Documentation

--- routes/commerces.js
+++ routes/commerces.js
@@ -1,15 +1,19 @@
 const express = require('express');
-const path = require('path');
 
 const commerces = require('../data/commerces.json');
 
 var router = express.Router();
 
-
+/**
+ * Récupère l'ensemble des commerces
+ */
 router.get('/', function(req, res){
     res.send(commerces)
 });
 
+/**
+ * Récupère le commerce ayant l'id :id. S'utilise comme /commerces/3
+ */
 router.get('/:id', function(req, res){
     res.send(commerces[req.params['id']]);
 });
--- routes/html.js
+++ routes/html.js
@@ -3,7 +3,12 @@ const path = require('path');
 
 var router = express.Router();
 
-
+/**
+ * Cette classe sert à retourner les pages HTML. Vous devez modifier cette classe pour ajouter les liens vers vos pages.
+ * Le premier paramètre devrait être / suivit du nom de votre page. Le second paramètre est une fonction anonyme. Le
+ * paramètre req représente la requête courante et res représente la réponse. La réponse retourne le fichier html demandé.
+ * Votre fichier devrait être dans le dossier client.
+ */
 router.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/../client/accueil.html'));
 });
