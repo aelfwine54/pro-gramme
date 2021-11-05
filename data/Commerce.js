096c08a750a2b3daac57e307d2b540dd07e33b4b
berf2608 <frederic.bergeron2@usherbrooke.ca>
2021-11-04 23:06:38 -0400

    Stub pour la suite

---
+++ data/Commerce.js
@@ -0,0 +1,14 @@
+class Commerce {
+    constructor (nom, ville, adresse, pays, horaire, site_web) {
+        this.nom = nom;
+        this.ville = ville;
+        this.adresse = adresse;
+        this.pays = pays;
+        this.horaire = horaire;
+        this.site_web = site_web;
+    }
+
+    
+}
+
+module.exports = Commerce;
